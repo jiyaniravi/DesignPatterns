684c4e2b35292c2b022aacc8ea31527416847035
Ravi Jiyani <jiyaniravi@gmail.com>
2020-08-21 13:11:09 +0530

    Dependency inversion - Tight coupling check-in

--- src/com/af/solid/d/Demo.java
+++ src/com/af/solid/d/Demo.java
@@ -1,6 +1,8 @@
 package com.af.solid.d;
 
 import java.util.ArrayList;
+import java.util.List;
+import org.javatuples.Triplet;
 
 enum Relationship{
     PARENT,
@@ -25,18 +27,38 @@ class Person{
 }
 
 class Relationships{
-    //private List<Triplet<Person, Relationship, Person>> relations = new ArrayList<Object>();
+    private List<Triplet<Person, Relationship, Person>> relations = new ArrayList<>();
+
+    public List<Triplet<Person, Relationship, Person>> getRelations() {
+        return relations;
+    }
 
     public void addParentAndChild(Person parent, Person child){
-        //relations.add();
+        relations.add(new Triplet<>(parent, Relationship.PARENT, child));
+        relations.add(new Triplet<>(child, Relationship.CHILD, parent));
+    }
+}
+
+class Researh{
+    public Researh(Relationships relationships) {
+        List<Triplet<Person, Relationship, Person>> relations = relationships.getRelations();
+        relations.stream()
+                .filter(x -> x.getValue0().getName().equals("John")
+                 && x.getValue1() == Relationship.PARENT)
+                .forEach(x -> System.out.println("John has a child called : "+x.getValue2().getName()));
     }
 }
 
 public class Demo {
     public static void main(String[] args) {
-        switch(5){
-            case 1,5 -> System.out.println("1,5");
-            case 2   -> System.out.println("2");
-        }
+        Person parent = new Person("John");
+        Person child1 = new Person("Chris");
+        Person child2 = new Person("Matt");
+
+        Relationships relationships = new Relationships();
+        relationships.addParentAndChild(parent, child1);
+        relationships.addParentAndChild(parent, child2);
+
+        new Researh(relationships);
     }
 }
