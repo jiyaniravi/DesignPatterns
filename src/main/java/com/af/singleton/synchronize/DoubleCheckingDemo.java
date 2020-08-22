02e5e455bf250bfb21df37c9f976a67067e2c183
Ravi Jiyani <jiyaniravi@gmail.com>
2020-08-22 16:03:20 +0530

    Double checked locking mechanism added while creating lazy singleton instance

--- src/main/java/com/af/singleton/synchronize/DoubleCheckingDemo.java
+++ src/main/java/com/af/singleton/synchronize/DoubleCheckingDemo.java
@@ -7,9 +7,29 @@ class LazySingleton{
         System.out.println("Lazy initialization of singleton object");
     }
 
-    public static LazySingleton getInstance(){
+    /*public static LazySingleton getInstance(){
         if(INSTANCE == null){
             return new LazySingleton();
+        }
+        return INSTANCE;
+    }*/
+
+    //Using synchronization
+    /*public synchronized static LazySingleton getInstance(){
+        if(INSTANCE == null){
+            return new LazySingleton();
+        }
+        return INSTANCE;
+    }*/
+
+    //Double checked locking
+    public static LazySingleton getInstance(){
+        if(INSTANCE == null){
+            synchronized (LazySingleton.class){
+                if(INSTANCE ==null){
+                    return new LazySingleton();
+                }
+            }
         }
         return INSTANCE;
     }
