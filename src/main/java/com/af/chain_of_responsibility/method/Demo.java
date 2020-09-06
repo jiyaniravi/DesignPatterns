c19b39b63fec84ca4bc88bcb04800950cc756030
Ravi Jiyani <jiyaniravi@gmail.com>
2020-09-06 19:00:51 +0530

    Chain of responsibility demo

---
+++ src/main/java/com/af/chain_of_responsibility/method/Demo.java
@@ -0,0 +1,104 @@
+package com.af.chain_of_responsibility.method;
+
+class Creature{
+    public String name;
+    public int attack, defense;
+
+    public Creature(String name, int attack, int defense) {
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    @Override
+    public String toString() {
+        return "Creature{" +
+                "name='" + name + '\'' +
+                ", attack=" + attack +
+                ", defense=" + defense +
+                '}';
+    }
+}
+
+class CreatureModifier{
+    protected Creature creature;
+    protected CreatureModifier next;
+
+    public CreatureModifier(Creature creature) {
+        this.creature = creature;
+    }
+
+    public void add(CreatureModifier cm){
+        if(next != null){
+            next.add(cm);
+        }else{
+            next = cm;
+        }
+    }
+
+    public void handle(){
+        if(next != null){
+            next.handle();
+        }
+    }
+}
+
+class DoubleAttackModifier extends CreatureModifier{
+
+    public DoubleAttackModifier(Creature creature) {
+        super(creature);
+    }
+
+    @Override
+    public void handle() {
+        System.out.println("Doubling "+creature.name+"'s attack");
+        creature.attack*=2;
+        super.handle();
+    }
+}
+
+class IncreaseDefenseModifier extends CreatureModifier{
+
+    public IncreaseDefenseModifier(Creature creature) {
+        super(creature);
+    }
+
+    @Override
+    public void handle() {
+        System.out.println("Increasing "+creature.name+"'s defense");
+        creature.defense+=3;
+        super.handle();
+    }
+}
+
+class NoBonusModifier extends CreatureModifier{
+
+    public NoBonusModifier(Creature creature) {
+        super(creature);
+    }
+
+    @Override
+    public void handle() {
+        System.out.println("No bonus for you!");
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Creature goblin = new Creature("Goblin", 2, 2);
+        System.out.println(goblin);
+
+        CreatureModifier root = new CreatureModifier(goblin);
+
+        root.add(new NoBonusModifier(goblin));
+
+        System.out.println("Let's double goblins attack!");
+        root.add(new DoubleAttackModifier(goblin));
+
+        System.out.println("Let's increase goblins defense!");
+        root.add(new IncreaseDefenseModifier(goblin));
+
+        root.handle();
+        System.out.println(goblin);
+    }
+}
