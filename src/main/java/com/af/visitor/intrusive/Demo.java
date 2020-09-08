b526bcc24e55ec8946936191bd6a07090057131a
Ravi Jiyani <jiyaniravi@gmail.com>
2020-09-08 23:04:26 +0530

    Intrusive Visitor design pattern > Not helpful as it breaks Open Close principle and Single responsibility principle

---
+++ src/main/java/com/af/visitor/intrusive/Demo.java
@@ -0,0 +1,50 @@
+package com.af.visitor.intrusive;
+
+abstract class Expression{
+    public abstract void print(StringBuilder sb);
+}
+
+class DoubleExpression extends Expression{
+    private double value;
+
+    public DoubleExpression(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public void print(StringBuilder sb) {
+        sb.append(value);
+    }
+}
+
+class AdditionExpression extends Expression{
+    private Expression left, right;
+
+    public AdditionExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public void print(StringBuilder sb) {
+        sb.append("(");
+        left.print(sb);
+        sb.append("+");
+        right.print(sb);
+        sb.append(")");
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        // 1+(2+3)
+        AdditionExpression additionExpression = new AdditionExpression(
+                new DoubleExpression(1),
+                new AdditionExpression(new DoubleExpression(2), new DoubleExpression(3))
+        );
+
+        StringBuilder sb = new StringBuilder();
+        additionExpression.print(sb);
+        System.out.println(sb.toString());
+    }
+}
