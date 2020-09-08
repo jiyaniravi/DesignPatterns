2c0e0135cfff838e1533744bf4ff62fca001dc00
Ravi Jiyani <jiyaniravi@gmail.com>
2020-09-08 17:30:04 +0530

    Event Handler added which contains subscriptions of an event.

---
+++ src/main/java/com/af/observer/event/Demo.java
@@ -0,0 +1,83 @@
+package com.af.observer.event;
+
+import java.lang.annotation.Target;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+class Event<TArgs>{
+    private int count = 0;
+    private Map<Integer, Consumer<TArgs>> handlers = new HashMap<>();
+
+    public Subscription addHandler(Consumer<TArgs> handler){
+        int i = count;
+        handlers.put(count++, handler);
+        return new Subscription(this, i);
+    }
+
+    public void fire(TArgs tArgs){
+        for(Consumer<TArgs> handler: handlers.values()){
+            handler.accept(tArgs);
+        }
+    }
+
+    public class Subscription implements AutoCloseable{
+
+        private Event<TArgs> event;
+        private int id;
+
+        public Subscription(Event<TArgs> event, int id) {
+            this.event = event;
+            this.id = id;
+        }
+
+        @Override
+        public void close(){
+            event.handlers.remove(this.id);
+        }
+    }
+}
+
+class PropertyChangedEventArgs{
+    public Object source;
+    public String propertyName;
+    public Object newValue;
+
+    public PropertyChangedEventArgs(Object source, String propertyName, Object newValue) {
+        this.source = source;
+        this.propertyName = propertyName;
+        this.newValue = newValue;
+    }
+}
+
+class Person{
+    public Event<PropertyChangedEventArgs> propertyChanged = new Event<>();
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(this.age == age) return;
+
+        this.age = age;
+        System.out.println("Set age to : "+age);
+        propertyChanged.fire(new PropertyChangedEventArgs(this, "age", age));
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        Event<PropertyChangedEventArgs>.Subscription sub = person.propertyChanged.addHandler(x ->
+                System.out.println("Person's " + x.propertyName + " has changed to " + x.newValue));
+
+        person.setAge(15);
+        person.setAge(25);
+        sub.close();
+        person.setAge(35);
+    }
+}
